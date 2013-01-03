04a6d9ff86a9f5bacec931e9a70f65ebfb00943d
HsoulT <alxanderr@gmail.com>
2013-01-03 18:29:21 -0400

    Movement Update
    Added time sensitive movement, and single block climbing. Need to add
    support for climbing more than one block.

--- Assets/Scripts/Character_Movement.cs
+++ Assets/Scripts/Character_Movement.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class Character_Movement : MonoBehaviour {
 
 	// Use this for initialization
+	private Vector3 Forward = new Vector3(0,0,-1); // saved this forward vector since used often
+	public float timetoMove; // variable to tweak how long you have to hold a direction to move
+	private timeDirection timeD; // variable that holds information on what direction is faced, and how long the button for that direction was held down
 	void Start () {
-	
+		timeD=new timeDirection(0f,"none");	//initializess the timeD varible, see bottom for structure
 	}
 	
+	//Update that doesn't take into account rotation
 	// Update is called once per frame
-	void Update () {
+	/*void Update () {
 	if(Input.GetButtonDown("Horizontal")){
 		float test = Input.GetAxis("Horizontal");
 		if (test>0){
@@ -26,4 +30,113 @@ public class Character_Movement : MonoBehaviour {
 			transform.Translate(transform.InverseTransformDirection(new Vector3(0,0,-1)));
 	}
 	}
+	*/
+	void Update()
+	{
+		Debug.DrawRay(transform.position,transform.TransformDirection(Forward),Color.red); //shows debug of ray collision, check scene view
+		if(Input.GetButton("Horizontal")) //checks left/right directional keys
+		{
+			float test = Input.GetAxis("Horizontal");//check if movement is along x-axis
+			if (test>0) //if right
+			{
+				transform.eulerAngles = new Vector3(0,-90,0); //turn towards right
+				if (timeD.direction!="right") //checks if last button was for right
+				{
+					timeD.time = 0; //resets time, and sets direction to right
+					timeD.direction = "right";
+					Debug.Log("right"); //debugs right in console
+				}
+				else
+				{
+					timeD.time += Time.deltaTime; //only situation where direction isn't right is if it is right, so adds up time to move
+				}				
+			}
+			else //same as last block, different direction
+			{
+				transform.eulerAngles = new Vector3(0,90,0);
+				if (timeD.direction!="left")
+				{
+					timeD.time = 0;
+					timeD.direction = "left";
+					Debug.Log("left");
+				}
+				else
+				{
+					timeD.time += Time.deltaTime;
+				}	
+			}
+			if(!Physics.Raycast(transform.position,transform.TransformDirection(Forward),1)&&timeD.time>timetoMove) //if there is no collision ahead, and the time is large enough then move
+				{
+					transform.Translate(Forward); //translate object in forward direction
+					timeD.time -= timetoMove; //subtracts time so unit doesn't move immediately 
+				}	
+			else if(Physics.Raycast(transform.position,transform.TransformDirection(Forward),1)&&timeD.time>timetoMove) //if collison then same as above, but also move up a unit
+			//this block needs to be updated to check for stacks higher than one ahead
+			{
+				transform.Translate(new Vector3(0,1,-1));
+				timeD.time -= timetoMove;
+			}		
+		}
+
+		if(Input.GetButton("Vertical")) //same as last if block, except checks up/down directional keys
+			{
+			float test = Input.GetAxis("Vertical");//check if movement is along z-axis
+			if (test>0)
+			{
+				transform.eulerAngles = new Vector3(0,180,0);
+				if (timeD.direction!="up")
+				{
+					timeD.time = 0;
+					timeD.direction = "up";
+					Debug.Log("up");
+				}
+				else
+				{
+					timeD.time += Time.deltaTime;
+				}				
+			}
+			else
+			{
+				transform.eulerAngles = new Vector3(0,0,0);
+				if (timeD.direction!="down")
+				{
+					timeD.time = 0;
+					timeD.direction = "down";
+					Debug.Log("down");
+				}
+				else
+				{
+					timeD.time += Time.deltaTime;
+				}				
+			}			
+			if(!Physics.Raycast(transform.position,transform.TransformDirection(Forward),1)&&timeD.time>timetoMove)
+				{
+					transform.Translate(Forward);
+					timeD.time -= timetoMove;
+				}
+			else if(Physics.Raycast(transform.position,transform.TransformDirection(Forward),1)&&timeD.time>timetoMove)
+			{
+				transform.Translate(new Vector3(0,1,-1));
+				timeD.time -= timetoMove;
+			}
+			}
+		if(!Input.GetButton("Vertical")&&!Input.GetButton("Horizontal")) //resets if no direction is held
+			timeD.time = 0f;			
+
+		Debug.Log(timeD.time);
+
+	}
+
+	public struct timeDirection //structure that holds direction of player, and time button is held for that direction
+	{
+		public float time;
+		public string direction;
+
+		public timeDirection(float x, string y) //initializer
+		{
+			time = x;
+			direction = y;
+		}
+	}
 }
+
