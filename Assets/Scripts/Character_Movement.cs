5a8619e22a8e9e0d3b857f5dd4f3150eb1b718c6
HsoulT <alxanderr@gmail.com>
2013-01-07 15:40:58 -0400

    Added simple ledge moving

--- Assets/Scripts/Character_Movement.cs
+++ Assets/Scripts/Character_Movement.cs
@@ -10,88 +10,90 @@ public class Character_Movement : MonoBehaviour {
 	public bool Hanging;
 	void Start () {
 		timeD=new timeDirection(0f,"none");	//initializess the timeD varible, see bottom for structure
-		Hanging = false; // doesn't do anything yet
+		Hanging = false; // doesn't do normal movement if hanging
 	}
 	
 	void Update()
-	{	
-		Debug.DrawRay(transform.position,transform.TransformDirection(Forward),Color.red); //shows debug of ray collision, check scene view
-		if(Input.GetButton("Horizontal") && !Input.GetButton("Grab")) //checks left/right directional keys and if grab isn't pressed
+	{
+		if(!Hanging)	
 		{
-			float test = Input.GetAxis("Horizontal");//check if movement is along x-axis
-			if (test>0) //if right
+			Debug.DrawRay(transform.position,transform.TransformDirection(Forward),Color.red); //shows debug of ray collision, check scene view
+			if(Input.GetButton("Horizontal") && !Input.GetButton("Grab")) //checks left/right directional keys and if grab isn't pressed
 			{
-				transform.eulerAngles = new Vector3(0,-90,0); //turn towards right
-				if (timeD.direction!="right") //checks if last button was for right
+				float test = Input.GetAxis("Horizontal");//check if movement is along x-axis
+				if (test>0) //if right
 				{
-					timeD.time = 0; //resets time, and sets direction to right
-					timeD.direction = "right";
-					Debug.Log("right"); //debugs right in console
+					transform.eulerAngles = new Vector3(0,-90,0); //turn towards right
+					if (timeD.direction!="right") //checks if last button was for right
+					{
+						timeD.time = 0; //resets time, and sets direction to right
+						timeD.direction = "right";
+						Debug.Log("right"); //debugs right in console
+					}
+					else
+					{
+						timeD.time += Time.deltaTime; //only situation where direction isn't right is if it is right, so adds up time to move
+					}				
 				}
-				else
+				else //same as last block, different direction
 				{
-					timeD.time += Time.deltaTime; //only situation where direction isn't right is if it is right, so adds up time to move
-				}				
-			}
-			else //same as last block, different direction
-			{
-				transform.eulerAngles = new Vector3(0,90,0);
-				if (timeD.direction!="left")
-				{
-					timeD.time = 0;
-					timeD.direction = "left";
-					Debug.Log("left");
-				}
-				else
-				{
-					timeD.time += Time.deltaTime;
-				}	
-			}		
+					transform.eulerAngles = new Vector3(0,90,0);
+					if (timeD.direction!="left")
+					{
+						timeD.time = 0;
+						timeD.direction = "left";
+						Debug.Log("left");
+					}
+					else
+					{
+						timeD.time += Time.deltaTime;
+					}	
+				}		
 
-			move();			
+				move();			
+			}
+
+			if(Input.GetButton("Vertical")&& !Input.GetButton("Grab")) //same as last if block, except checks up/down directional keys
+				{
+				float test = Input.GetAxis("Vertical");//check if movement is along z-axis
+				if (test>0)
+				{
+					transform.eulerAngles = new Vector3(0,180,0);
+					if (timeD.direction!="up")
+					{
+						timeD.time = 0;
+						timeD.direction = "up";
+						Debug.Log("up");
+					}
+					else
+					{
+						timeD.time += Time.deltaTime;
+					}				
+				}
+				else
+				{
+					transform.eulerAngles = new Vector3(0,0,0);
+					if (timeD.direction!="down")
+					{
+						timeD.time = 0;
+						timeD.direction = "down";
+						Debug.Log("down");
+					}
+					else
+					{
+						timeD.time += Time.deltaTime;
+					}				
+				}			
+				
+				move();
+
+				}
+			if(!Input.GetButton("Vertical")&&!Input.GetButton("Horizontal") || Input.GetButton("Grab")) //resets if no direction is held or grab held
+				timeD.time = 0f;			
+
+			if(timeD.time!=0)
+				Debug.Log(timeD.time);
 		}
-
-		if(Input.GetButton("Vertical")&& !Input.GetButton("Grab")) //same as last if block, except checks up/down directional keys
-			{
-			float test = Input.GetAxis("Vertical");//check if movement is along z-axis
-			if (test>0)
-			{
-				transform.eulerAngles = new Vector3(0,180,0);
-				if (timeD.direction!="up")
-				{
-					timeD.time = 0;
-					timeD.direction = "up";
-					Debug.Log("up");
-				}
-				else
-				{
-					timeD.time += Time.deltaTime;
-				}				
-			}
-			else
-			{
-				transform.eulerAngles = new Vector3(0,0,0);
-				if (timeD.direction!="down")
-				{
-					timeD.time = 0;
-					timeD.direction = "down";
-					Debug.Log("down");
-				}
-				else
-				{
-					timeD.time += Time.deltaTime;
-				}				
-			}			
-			
-			move();
-
-			}
-		if(!Input.GetButton("Vertical")&&!Input.GetButton("Horizontal") || Input.GetButton("Grab")) //resets if no direction is held or grab held
-			timeD.time = 0f;			
-
-		if(timeD.time!=0)
-			Debug.Log(timeD.time);
-	
 
 		}
 
--- Assets/Scripts/Character_Movement_Ledge.cs
+++ Assets/Scripts/Character_Movement_Ledge.cs
@@ -11,15 +11,18 @@ public class Character_Movement_Ledge : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(!Physics.Raycast(transform.position,new Vector3(0,-1,0),1)&&Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,0,1)),1))
+		
+		if(!Physics.Raycast(transform.position,new Vector3(0,-1,0),1)&&Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,0,1)),1)
+			&& !mov.Hanging)
 		{
 			mov.Hanging = true;
+			transform.Rotate(new Vector3(0,180,0));
 			rigidbody.useGravity = false;
 			rigidbody.velocity = new Vector3(0,0,0);
 
-		}
+		}		
 
-		else if(!Physics.Raycast(transform.position,new Vector3(0,-1,0),1))
+		else if(!Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,0,-1)),1)&& mov.Hanging)
 		{
 			mov.Hanging = false;
 			rigidbody.useGravity = true;
@@ -27,7 +30,22 @@ public class Character_Movement_Ledge : MonoBehaviour {
 
 		if(mov.Hanging)
 		{
+			if (Input.GetButton("Horizontal"))
+			{
+				float test = Input.GetAxis("Horizontal");
+				{
+					if (test>0)
+					{
+						transform.Translate(transform.TransformDirection(new Vector3(1,0,0)));
+					}
 
+					else
+					{
+						transform.Translate(transform.TransformDirection(new Vector3(-1,0,0)));
+
+					}
+				}
+			}
 		}		
 	}
 }
