59843d5a37d9bf13855b791b0d1f9db677a52185
Max <max_wawer@hotmail.com>
2013-01-09 18:55:35 -0400

    I did a bunch
    I touched up the system for autograbbing blocks
    I fixed spidering to correctly switch blocks round corners
    blocks fall
    I tried animating things

--- Assets/Scripts/Block_Move.cs
+++ Assets/Scripts/Block_Move.cs
@@ -5,11 +5,16 @@ public class Block_Move : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-	Transform curTransform;
-        curTransform = gameObject.GetComponent<Transform>();
-        curTransform = gameObject.transform;
+	//Transform curTransform;
+      //  curTransform = gameObject.GetComponent<Transform>();
+        //curTransform = gameObject.transform;
 	}
 	
+	public bool chosen = false;
+	bool grabbed = false;
+	public AnimationState grab;
+	public AnimationState letGo;
+		
 	// Update is called once per frame
 	void Update () {
 
@@ -21,30 +26,83 @@ public class Block_Move : MonoBehaviour {
 		RaycastHit right;
 		RaycastHit back;
 		Vector3 direction;
-
+		
+		
+		
+		//why does this not trigger when pressing grab to stop hanging
+		
+		//chosen bool? that's reset upon upSpace
+		
+		//check your direction, if facing right take left-side vector priority. If facing left take right-side vector priority.
+		
+		//when you rotate, it grabs your initial direction for use of pushing
+		//timeD not updated because button not pressed. How to know which direction now facing
+		
+		
 		if(Input.GetButton("Grab"))
 		{
-			
+			if(!grabbed)
+			{
+				//animation.Play ("grab2");
+				grabbed = true;
+			}
+			if(!chosen)
+			{
 			if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,0,-1)),out front,1))
 				{
+				//unnecessary just for clarity
 					transform.Rotate(new Vector3(0,0,0));
+					chosen = true;
 				}
-			
-			else if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(-1,0,0)),out right,1))
-				{
-					transform.Rotate(new Vector3(0,-90,0));
+				
+				else{
+				
+					if(other.timeD.direction == "right" || other.timeD.direction == "up")
+					{
+						if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(1,0,0)),out left,1))
+						{
+							transform.Rotate(new Vector3(0,-90,0));
+							chosen = true;
+							RotateTimeD("l", ref other);
+						}
+					else if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(-1,0,0)),out right,1))
+						{
+							transform.Rotate(new Vector3(0,90,0));
+							chosen = true;
+							RotateTimeD("r", ref other);
+						}
+					else if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,0,1)),out back,1))
+						{
+							transform.Rotate(new Vector3(0,180,0));
+							chosen = true;
+							RotateTimeD("d", ref other);
+						}
+					}
+				
+					else //left/down
+					{
+						if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(-1,0,0)),out right,1))
+						{
+							transform.Rotate(new Vector3(0,90,0));
+							chosen = true;
+							RotateTimeD("r", ref other);
+						}
+					
+					else if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(1,0,0)),out left,1))
+						{
+							transform.Rotate(new Vector3(0,-90,0));
+							chosen = true;
+							RotateTimeD("l", ref other);
+						}
+					else if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,0,1)),out back,1))
+						{
+							transform.Rotate(new Vector3(0,180,0));
+							chosen = true;
+							RotateTimeD("d", ref other);
+						}
+			}	
 				}
-			
-			else if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(1,0,0)),out left,1))
-				{
-					transform.Rotate(new Vector3(0,90,0));
-				}
-			
-			else if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,0,1)),out back,1))
-				{
-					transform.Rotate(new Vector3(0,180,0));
-				}
-			
+			}
 			if(Input.GetButtonDown("Horizontal"))
 			{
 				float test = Input.GetAxis("Horizontal");
@@ -58,6 +116,9 @@ public class Block_Move : MonoBehaviour {
 							direction = new Vector3(-1,0,0);}						
 
 						front.transform.Translate(direction);
+						
+						if(Physics.Raycast(transform.position + transform.TransformDirection(new Vector3(0,0,-1)),new Vector3(0,-1,0),1))
+							other.pushStep(test);
 					}
 				}
 			}
@@ -75,11 +136,60 @@ public class Block_Move : MonoBehaviour {
 							direction = new Vector3(0,0,-1);}	
 
 						front.transform.Translate(direction);
+						
+						if(Physics.Raycast(transform.position + transform.TransformDirection(new Vector3(0,0,-1)),new Vector3(0,-1,0),1))
+							other.pushStep(test);
 					}
 				}
 			}
 		}
+		
+		if(Input.GetButtonUp("Grab"))
+		{
+			chosen = false;
+			//animation.Play ("letGo");
+			grabbed = false;
+		}
 	
 	}
 	
+	void RotateTimeD(string dir, ref Character_Movement other)
+	{
+		if(dir== "l")
+		{
+			if(other.timeD.direction=="up")
+				other.timeD.direction="left";
+			else if(other.timeD.direction=="left")
+				other.timeD.direction="down";
+			else if(other.timeD.direction=="down")
+				other.timeD.direction="right";
+			else if(other.timeD.direction=="right")
+				other.timeD.direction="up";
+		}
+		
+		if(dir== "r")
+		{
+			if(other.timeD.direction=="up")
+				other.timeD.direction="right";
+			else if(other.timeD.direction=="left")
+				other.timeD.direction="up";
+			else if(other.timeD.direction=="down")
+				other.timeD.direction="left";
+			else if(other.timeD.direction=="right")
+				other.timeD.direction="down";
+		}
+		
+		if(dir== "d")
+		{
+			if(other.timeD.direction=="up")
+				other.timeD.direction="down";
+			else if(other.timeD.direction=="left")
+				other.timeD.direction="right";
+			else if(other.timeD.direction=="down")
+				other.timeD.direction="up";
+			else if(other.timeD.direction=="right")
+				other.timeD.direction="left";
+		}
+	}
+
 }
--- Assets/Scripts/Character_Movement_Ledge.cs
+++ Assets/Scripts/Character_Movement_Ledge.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class Character_Movement_Ledge : MonoBehaviour {
 	
+	private Vector3 diagLeft = new Vector3(1,1,0);
+	private Vector3 diagRight = new Vector3(-1,1,0);
+	private Vector3 diagRightF = new Vector3(-1,1,-1);
+	private Vector3 diagLeftF = new Vector3(1,1,-1);
+	
 	private Character_Movement mov;
 	// Use this for initialization
 	void Start () {
@@ -11,6 +16,12 @@ public class Character_Movement_Ledge : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {	
+		
+		//Debug.DrawRay(transform.position,transform.TransformDirection(diagRight),Color.magenta);
+		Debug.DrawRay(transform.position,transform.TransformDirection(diagLeft),Color.magenta);
+		//Debug.DrawRay(transform.position,transform.TransformDirection(diagRightF),Color.magenta);
+		Debug.DrawRay(transform.position,transform.TransformDirection(diagLeftF),Color.magenta);
+		
 		if(!Physics.Raycast(transform.position,new Vector3(0,-1,0),1)&&Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,0,1)),1)
 			&& !mov.Hanging)
 		{
@@ -30,41 +41,68 @@ public class Character_Movement_Ledge : MonoBehaviour {
 		if(mov.Hanging)
 		{
 			Debug.DrawRay(transform.position+transform.TransformDirection(new Vector3(0,0,-1)),transform.TransformDirection(new Vector3(1,0,0)),Color.red); //shows debug of ray collision, check scene view
-
 			if (Input.GetButtonDown("Horizontal"))
 			{
 				float test = Input.GetAxis("Horizontal");
 				{
-					if (test>0)
+					//still go through blocks when pressing right. As well, sometimes unresponsive.
+					if (test>0 && !(Physics.Raycast(transform.position,diagRight,1) || Physics.Raycast(transform.position,diagRightF,1)))
 					{
-						if(Physics.Raycast(transform.position+transform.TransformDirection(new Vector3(0,0,-1)),transform.TransformDirection(new Vector3(-1,0,0)),1))
+						if(Physics.Raycast(transform.position, -transform.right,1))
+							transform.Rotate(0,90,0);
+						else if(Physics.Raycast(transform.position+transform.TransformDirection(new Vector3(0,0,-1)),transform.TransformDirection(new Vector3(-1,0,0)),1))
 							transform.Translate(new Vector3(-1,0,0));
 						else
-							transform.RotateAround(transform.position+transform.TransformDirection(new Vector3(0,0,-1)),new Vector3(0,1,0),270);
+						{
+							transform.RotateAround(transform.position+transform.TransformDirection(new Vector3(0,0,-1)),new Vector3(0,1,0),-90);
+						}
 					}
 
-					else
+					else if (test<0 && !(Physics.Raycast(transform.position,diagLeft,1) || Physics.Raycast(transform.position,diagLeftF,1)))
 					{
-						if(Physics.Raycast(transform.position+transform.TransformDirection(new Vector3(0,0,-1)),transform.TransformDirection(new Vector3(1,0,0)),1))
+						if(Physics.Raycast(transform.position, transform.right,1))
+							transform.Rotate(0,-90,0);
+						else if(Physics.Raycast(transform.position+transform.TransformDirection(new Vector3(0,0,-1)),transform.TransformDirection(new Vector3(1,0,0)),1))
 							transform.Translate(new Vector3(1,0,0));
 						else
+							//rotating around the wrong object. Also rotating 180 degrees. 
 							transform.RotateAround(transform.position+transform.TransformDirection(new Vector3(0,0,-1)),new Vector3(0,1,0),90);
 					}
 				}
 
 			}
 
+			if(Input.GetButton("Grab"))
+				{
+					if(Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,-1,0)),1))
+						{
+							transform.Translate(new Vector3(0,-1f,0));
+							mov.Hanging = false;
+							rigidbody.useGravity = true;
+						}
+				}
+			
+			if(Input.GetButtonDown("Release"))
+			{
+				mov.Hanging = false;
+				rigidbody.useGravity = true;
+				while(!Physics.Raycast(transform.position,transform.TransformDirection(new Vector3(0,-1,0)),1))
+				{
+					transform.Translate(new Vector3(0,-1f,0));
+				}	
+			}
+			
 			if (Input.GetButtonDown("Vertical"))
 				{
 					float test = Input.GetAxis("Vertical");
 
-					if (test>0)
+					if (test > 0 && !Physics.Raycast(transform.position-transform.forward,new Vector3(0,1,0),1))
 						{
 							transform.Translate(new Vector3(0,0.1f,-1));
 							mov.Hanging = false;
 							rigidbody.useGravity = true;
 						}
-					else
+					else if(test < 0)
 					{
 						transform.Translate(new Vector3(0,-1f,0));
 						mov.Hanging = false;
