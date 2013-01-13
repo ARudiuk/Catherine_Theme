b0b1fe3244601702fa78395a0ba304faea6238db
HsoulT <alxanderr@gmail.com>
2013-01-12 20:52:55 -0400

    Basic camera moving
    Camera can move 45deg in either direction around the y-axis of the character

--- Assets/Scripts/Camera_Follow.cs
+++ Assets/Scripts/Camera_Follow.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
 using System.Collections;
 
-public class Camera_Follow : MonoBehaviour {
+public class Camera_Follow : MonoBehaviour {	
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{		
 	}
-	
 	// Update is called once per frame
 	void Update () {
+
 		transform.LookAt(GameObject.Find("crappy_character").transform);
+		//Camera turns 45deg around the character depending on whethere q/e are pressed
+		if(Input.GetButtonDown("Camera"))
+		{
+			float test = Input.GetAxis("Camera");
+			GameObject hold = GameObject.Find("crappy_character");
+
+			if(test>0)
+			{
+				transform.RotateAround(hold.transform.position,Vector3.up,-45);
+			}
+
+			else
+			{
+				transform.RotateAround(hold.transform.position,Vector3.up,45);
+			}
+		}		
+
 	}
 }
