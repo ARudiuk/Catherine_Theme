1f6a5411bae5006e325ace6cfc63eb8e90b86437
HsoulT <alxanderr@gmail.com>
2013-01-27 23:05:28 -0400

    Added skeleton for timer

---
+++ Assets/Scripts/end_Timer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class end_Timer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
